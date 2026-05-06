50ea38c601c4489e53025e7283539167c9c3d8fc
siamparvez001 <siamparvez003@gmail.com>
2026-05-06 14:30:32 +0600

    email password auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,12 +2,15 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("pixgen");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
+    database: mongodbAdapter(db, {
 
-    client
-  }),
+        client
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
 });
